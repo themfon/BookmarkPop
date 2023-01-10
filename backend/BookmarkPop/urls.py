1aa51ecf11e0fdaccd5fa19e8f8028bb31e434f5
0papi <evanskwofie67@gmail.com>
2023-01-10 16:13:42 +0000

    remove python backend files

--- backend/BookmarkPop/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from BookmarkPop import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-]
--- backend/BookmarkPop/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-#Create views here
-
-def home(request):
-    return HttpResponse("👋🏽")
